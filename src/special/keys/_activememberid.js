1e54d77ff0e4b70c7a1a8fd99c358e5393cf4dd5
raleigh <raleighed@proton.me>
2025-08-30 16:14:56 +0100

    activ member improvement

--- src/special/keys/_activememberid.js
+++ src/special/keys/_activememberid.js
@@ -1,3 +1,23 @@
+function determineActiveValue(member, roles) {
+  var {
+    messages = 0,
+    highestroleorder = 0,
+    lastmessage = 0,
+    bot = false
+  } = member
+
+  var daysSinceLastActivity = (Date.now() - lastmessage) / (24 * 60 * 60 * 1000)
+  var recencyPenalty = Math.pow(0.5, daysSinceLastActivity / 7)
+
+  var messageWeight = Math.pow(roundTo(messages, 50) * highestroleorder / roles * 5, 1.5)
+
+  var botMultiplier = bot ? 0.5 : 1
+
+  var score = messageWeight * recencyPenalty * botMultiplier
+
+  return Math.max(score, 0.1)
+}
+
 module.exports = {
   desc: "Returns a random active member's ID from the server, this is calculated by the number of messages each one has sent and role order.",
   func: function (msg) {
@@ -5,40 +25,43 @@ module.exports = {
     let data = poopy.data
 
     var datamembers = data.guildData[msg.guild.id].allMembers
-    var keys = Object.keys(datamembers)
-
     var roles = msg.guild.roles?.cache?.size || 1
 
-    var determineActiveValue = (id) => Math.max(
-      Math.min(
-        ((datamembers[id].messages || 0) + (25 * (datamembers[id].highestroleorder || 0))) * (datamembers[id].bot ? 0.1 : 1) || 0,
-        100 * roles
-      ) - Math.floor((Date.now() - datamembers[id].lastmessage || 0) / 604800000) * 10 * roles,
-      0
-    ) || 0
+    var activeMembers = Object.entries(datamembers).filter(([_, member]) => {
+      var hasRecentActivity = (Date.now() - (member.lastmessage || 0)) < 30 * 24 * 60 * 60 * 1000
+      var hasMessages = (member.messages || 0) > 0
+      return hasRecentActivity && hasMessages
+    })
 
-    var sum = 0
-    for (var id in datamembers) {
-      sum += determineActiveValue(id)
+    if (activeMembers.length === 0) return ""
+
+    var weightedMembers = activeMembers.map(([id, member]) => {
+      var weight = determineActiveValue(member, roles)
+      return { id, member, weight }
+    }).sort((a, b) => b.weight - a.weight)
+
+    var totalWeight = weightedMembers.reduce((sum, wm) => sum + wm.weight, 0)
+
+    if (totalWeight === 0) {
+      var randomIndex = Math.floor(Math.random() * weightedMembers.length)
+      return weightedMembers[randomIndex].id
     }
 
-    var rnd = Math.random() * sum
-    var counter = 0
-
-    for (var id in datamembers) {
-      counter += determineActiveValue(id)
-      if (counter > rnd) {
-        return id
+    var random = Math.random() * totalWeight
+    for (var wm of weightedMembers) {
+      random -= wm.weight
+      if (random <= 0) {
+        return wm.id
       }
     }
 
-    return keys[0]
+    return weightedMembers[0].id
   },
   array: function (msg) {
     let poopy = this
     let data = poopy.data
 
-    var datamembers = data.guildData[msg.guild.id].allMembers;
+    var datamembers = data.guildData[msg.guild.id].allMembers
     return Object.keys(datamembers)
   }
 }
