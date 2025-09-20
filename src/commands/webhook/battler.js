5e8cb77c22a8e09d2d986328bf79137321032504
raleigh <58990470+raleighed@users.noreply.github.com>
2025-09-20 22:39:07 +0100

    p:battler logic

--- src/commands/webhook/battler.js
+++ src/commands/webhook/battler.js
@@ -32,20 +32,15 @@ module.exports = {
         }
 
         if (!data.guildData[msg.guild.id].channels[msg.channel.id].battling) {
-            if (msg.member.permissions.has(DiscordTypes.PermissionFlagsBits.ManageGuild) || msg.member.permissions.has(DiscordTypes.PermissionFlagsBits.ManageWebhooks) || msg.member.permissions.has(DiscordTypes.PermissionFlagsBits.Administrator) || msg.author.id === msg.guild.ownerID || config.ownerids.find(id => id == msg.author.id)) {
-                var battleValue = args[1] ? args[1].toLowerCase() : "battler"
+            var battleValue = args[1] ? args[1].toLowerCase() : "battler"
 
-                if (battleValue == "enemy" || battleValue == "enemies" || battleValue == "2") battleValue = 2
-                else battleValue = 1
+            if (battleValue == "enemy" || battleValue == "enemies" || battleValue == "2") battleValue = 2
+            else battleValue = 1
 
-                data.guildData[msg.guild.id].channels[msg.channel.id].battling = battleValue
+            data.guildData[msg.guild.id].channels[msg.channel.id].battling = battleValue
 
-                if (!msg.nosend) await msg.reply("https://static.wikia.nocookie.net/the-battle-bricks/images/0/03/TBB_current_logo.png").catch(() => { })
-                return "https://static.wikia.nocookie.net/the-battle-bricks/images/0/03/TBB_current_logo.png"
-            } else {
-                await msg.reply('You need to have the manage webhooks/messages permission to execute that!').catch(() => { })
-                return;
-            };
+            if (!msg.nosend) await msg.reply("https://static.wikia.nocookie.net/the-battle-bricks/images/0/03/TBB_current_logo.png").catch(() => { })
+            return "https://static.wikia.nocookie.net/the-battle-bricks/images/0/03/TBB_current_logo.png"
         } else {
             data.guildData[msg.guild.id].channels[msg.channel.id].battling = 0
 
@@ -58,7 +53,11 @@ module.exports = {
         value: "I'm Battler, and I'm always battling!"
     },
     cooldown: 2500,
-    perms: ['Administrator',
-        'ManageMessages'],
+    perms: [
+        'Administrator',
+        'ManageMessages',
+        'ManageWebhooks',
+        'ManageGuild'
+    ],
     type: 'Webhook'
 }
