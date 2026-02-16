0518b9e244d597669a1ee416244b946e42f4499d
raleigh <raleighed@proton.me>
2026-02-16 04:48:13 +0000

    include funcs i guess

---
+++ src/special/functions/globalincludesindex.js
@@ -0,0 +1,19 @@
+module.exports = {
+    helpf: '(arrayName | val)',
+    desc: 'Returns the index of the value if the global array includes it.',
+    func: async function (matches, msg, isBot, _, opts) {
+        let poopy = this
+        let { splitKeyFunc, getKeywordsFor } = poopy.functions
+        let tempdata = poopy.tempdata
+
+        var word = matches[1]
+        var split = splitKeyFunc(word, { args: 2 })
+        var name = await getKeywordsFor(split[0] ?? '', msg, isBot, opts).catch(() => { }) ?? ''
+        var val = split[1] ?? ''
+
+        var array = tempdata[msg.guild.id][msg.channel.id].arrays[name]
+        if (!array) return ''
+
+        return array.indexOf(val)
+    }
+}
---
+++ src/special/functions/includes.js
@@ -0,0 +1,20 @@
+module.exports = {
+    helpf: '(arrayName | val)',
+    desc: 'Returns the value if the array includes it.',
+    func: async function (matches, msg, isBot, _, opts) {
+        let poopy = this
+        let { splitKeyFunc, getKeywordsFor } = poopy.functions
+        let tempdata = poopy.tempdata
+
+        var word = matches[1]
+        var split = splitKeyFunc(word, { args: 2 })
+        var name = await getKeywordsFor(split[0] ?? '', msg, isBot, opts).catch(() => { }) ?? ''
+        var val = split[1] ?? ''
+
+        var array = tempdata[msg.author.id][msg.id].arrays[name]
+        if (!array) return ''
+
+        return array.includes(val) ? val : ''
+    },
+    raw: true
+}
