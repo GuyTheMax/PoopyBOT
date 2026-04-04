44c5b8ed0b8a9cfd506ab76c9bea3d9ad1763a7e
raleigh <58990470+raleighed@users.noreply.github.com>
2026-04-04 17:03:40 +0100

    the keyword

---
+++ src/special/keys/_tsunamiwave.js
@@ -0,0 +1,16 @@
+module.exports = {
+  desc: 'Returns a random wave from THE "TSUNAMI GAME", how WACKY! 🤪',
+  func: function () {
+    let poopy = this
+    let json = poopy.json
+    let { randomChoice } = poopy.functions
+
+    return randomChoice(json.tsunamiJSON).name
+  },
+  array: function () {
+    let poopy = this
+    let json = poopy.json
+
+    return json.tsunamiJSON.map(t => t.Name)
+  }
+}
