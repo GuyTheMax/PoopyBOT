3e37d65bb27b2b3b01538ba1f43b2b3fad2b997f
Emmie <81695336+EmmieShayz@users.noreply.github.com>
2026-06-04 19:27:37 +0400

    texting

---
+++ src/commands/conversion/txtfile.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: ['txtfile', 'textfile'],
+    args: [
+        { name: "text", required: true, specifarg: false, orig: "<text>" }
+    ],
+    execute: async function (msg, args) {
+        let poopy = this
+        let { fs, Discord } = poopy.modules
+        let vars = poopy.vars
+        let config = poopy.config
+
+        var saidMessage = args.slice(1).join(' ')
+        if (args[1] === undefined) {
+            saidMessage = ''
+        }
+        
+        var currentcount = vars.filecount
+        vars.filecount++
+        var filepath = `temp/${config.database}/file${currentcount}`
+        fs.mkdirSync(`${filepath}`)
+        fs.writeFileSync(`${filepath}/output.txt`, saidMessage)
+
+        return await sendFile(msg, filepath, 'output.txt')
+    },
+    help: {
+        name: 'txtfile/textfile <text>',
+        value: 'Converts the input into a txt file.'
+    },
+    cooldown: 1000,
+    type: 'Conversion'
+}
