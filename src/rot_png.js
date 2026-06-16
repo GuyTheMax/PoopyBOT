e23648d3673989aa8ede3b040b4615a89665bbf8
emmie <sannnnnes0@gmail.com>
2026-06-16 12:35:16 +0400

    what am i even doing

--- src/rot_png.js
+++ src/rot_png.js
@@ -18,9 +18,29 @@ const filter_remapping = {
   [PNG_FILTER_VALUE_PAETH]: PNG_FILTER_VALUE_AVG
 }
 
-const RANDOM_REMAPPING_CHANCE = 0.05
+const RANDOM_REMAPPING_CHANCE = 0.01
+const ROW_REVERSE_CHANCE = 0.05
+const ROW_SHUFFLE_CHANCE = 0.001
+const ROW_SQUARED_CHANCE = 0.01
+const ROW_CHANNEL_SWAP_CHANCE = 0.02
 
 let rottingChance = 0;
+
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
 
 export function setup(args)
 {
@@ -39,49 +59,44 @@ export function setup(args)
 export function glitch_frame(frame, stream)
 {
   const rows = frame.idat?.rows;
-  const originalRows = rows.slice();
-  const filterTypesOriginal = [];
-  const filterTypes = [];
+  
   console.log(rows?.length)
   console.log(rottingChance)
-  console.log('filter type:', filter_type)
   if ( !rows )
     return;
+
   const length = rows.length;
   for ( let i = 0; i < length; i++ )
   {
     const row = rows[i];
-    originalRows[i] = row.slice()
+    
     if (Math.random() > rottingChance) continue
 
     let newFilterType = filter_remapping[row[0]]
     if (Math.random() < RANDOM_REMAPPING_CHANCE)
       newFilterType = Math.floor(Math.random() * 4) + 1;
 
-    filterTypesOriginal.push(row[0])
     row[0] = newFilterType;
-    filterTypes.push(row[0])
-  }
 
-  console.log('Checking if every row is still the same for some reason')
-  var same = true
-  for ( let i = 0; i < length; i++ )
-  {
-    const rowA = originalRows[i]
-    const rowB = rows[i]
+    if (Math.random() < ROW_REVERSE_CHANCE)
+      rows[i] = [row[0]].concat(row.slice(1).reverse());
 
-    for ( let j in rowA ) {
-      if (rowA[j] !== rowB[j]) {
-        same = false
-        break;
+    if (Math.random() < ROW_SHUFFLE_CHANCE)
+      rows[i] = [row[0]].concat(shuffle(row.slice(1)));
+
+    if (Math.random() < ROW_SQUARED_CHANCE)
+      rows[i] = [row[0]].concat(row.slice(1).map(value => Math.pow(value, 2) % 256));
+
+    if (Math.random() < ROW_CHANNEL_SWAP_CHANCE) {
+      const rowModified = row.slice(1)
+
+      for ( let i = 0; i < rowModified.length; i += 4 ) {
+        // R<->B G<->A
+        [rowModified[i], rowModified[i+2]] = [rowModified[i+2], rowModified[i]]
+        [rowModified[i+1], rowModified[i+3]] = [rowModified[i+3], rowModified[i+1]]
       }
+
+      rows[i] = [row[0]].concat(rowModified)
     }
-
-    if (same == false)
-      break;
   }
-
-  console.log('Is the same:', same)
-  console.log('Original filter types:', filterTypesOriginal)
-  console.log('Filter types after processing:', filterTypes)
 }
